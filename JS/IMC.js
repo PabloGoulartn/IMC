99100376e80ca54434af9c30aa0f21b866576f76
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-29 21:59:47 -0300

    Adição do Action no form

--- JS/IMC.js
+++ JS/IMC.js
@@ -1,1 +1,5 @@
-alert((70 / Math.pow(170, 2) * 10000).toFixed(2))
+let IMC = (70 / Math.pow(170, 2) * 10000).toFixed(2)
+
+document.querySelector('.calculate-btn').addEventListener('click', function(){
+    document.querySelector('textarea').textContent = 'Ana seu IMC é ' + IMC;
+});
