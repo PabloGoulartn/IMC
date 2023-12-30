374db91862dc76da376f64a3108f750731ac030a
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-29 23:09:10 -0300

    IMC retorna se esta acima ou abaixo do peso

--- JS/IMC.js
+++ JS/IMC.js
@@ -1,5 +1,34 @@
-let IMC = (70 / Math.pow(170, 2) * 10000).toFixed(2)
+function getUserName()
+{
+    return document.querySelector('#nameInput').value;
+}
+
+function getTextArea()
+{
+    return document.querySelector('textarea');
+}
+
+function calculateIMC()
+{
+    const weight = document.querySelector('#weightInput').value;
+    const height = document.querySelector('#heightInput').value;
+
+    if (weight && height)
+        return (weight / Math.pow(height, 2) * 10000).toFixed(2);
+    else
+    {
+        getTextArea().textContent = '';
+        return 'Preencha os campos';
+    }
+}
 
 document.querySelector('.calculate-btn').addEventListener('click', function(){
-    document.querySelector('textarea').textContent = 'Ana seu IMC é ' + IMC;
+    if (calculateIMC() > 24.99)
+    {
+        getTextArea().textContent = `${getUserName()}, seu IMC é ${calculateIMC()}. Você está acima do peso. `;
+    }
+    else if (calculateIMC() < 18.50)
+    {
+        getTextArea().textContent = `${getUserName()}, seu IMC é ${calculateIMC()}. Você está abaixo do peso. `;
+    }
 });
